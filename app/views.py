684766a49028287a95dc3fd6e052bb0efa6f3844
DexMor <dexmor93@gmail.com>
2016-12-11 01:29:07 +0300

    Login

--- app/views.py
+++ app/views.py
@@ -368,3 +368,24 @@ def user_profile(request):
         args['form'] = form
     return render(request, 'blog/user_profile.html', args)
 
+
+def login(request):
+    args={}
+    args.update(csrf(request))
+    if request.POST:
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            return render_to_response('login.html', args)
+
+    else:
+        return render_to_response('login.html', args)
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
