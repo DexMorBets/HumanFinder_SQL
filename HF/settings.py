df96fa173825f406cf904c00595508e661b2ec60
DexMor <dexmor93@gmail.com>
2016-12-10 22:27:32 +0300

    migrations

--- HF/settings.py
+++ HF/settings.py
@@ -78,14 +78,22 @@ WSGI_APPLICATION = 'HF.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'humanfinder',
+#         'USER': 'admin',
+#         'PASSWORD': 'admin',
+#         'HOST': '',
+#         'PORT': '',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'humanfinder',
-        'USER': 'admin',
-        'PASSWORD': 'admin',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
